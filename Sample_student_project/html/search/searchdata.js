096d85b30f25db6e5b4f6b2741500ab45a571374
hahne29 <eh337414@ohio.edu>
2018-02-25 17:27:56 -0500

    running doxygen

--- Sample_student_project/html/search/searchdata.js
+++ Sample_student_project/html/search/searchdata.js
@@ -1,42 +1,12 @@
 var indexSectionsWithContent =
 {
-<<<<<<< HEAD
-  0: "gop",
-  1: "gop"
-=======
-<<<<<<< HEAD
-  0: "gop",
-  1: "gop"
-=======
->>>>>>> 731e3373baaf3f8b497d3ebf8294d88f708cd837
->>>>>>> fc4d5a446b437ee8a70ae75d626661d3368554bb
 };
 
 var indexSectionNames =
 {
-<<<<<<< HEAD
-  0: "all",
-  1: "classes"
-=======
-<<<<<<< HEAD
-  0: "all",
-  1: "classes"
-=======
->>>>>>> 731e3373baaf3f8b497d3ebf8294d88f708cd837
->>>>>>> fc4d5a446b437ee8a70ae75d626661d3368554bb
 };
 
 var indexSectionLabels =
 {
-<<<<<<< HEAD
-  0: "All",
-  1: "Classes"
-=======
-<<<<<<< HEAD
-  0: "All",
-  1: "Classes"
-=======
->>>>>>> 731e3373baaf3f8b497d3ebf8294d88f708cd837
->>>>>>> fc4d5a446b437ee8a70ae75d626661d3368554bb
 };
 
